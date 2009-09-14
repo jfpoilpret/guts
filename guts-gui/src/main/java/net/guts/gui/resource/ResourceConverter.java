96211d6d7bbc2d514f8fd27c8e909564815ef346
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2009-09-14 23:33:25 +0000

    Initial work on resource management, still under heavy work.

---
+++ guts-gui/src/main/java/net/guts/gui/resource/ResourceConverter.java
@@ -0,0 +1,77 @@
+//  Copyright 2009 Jean-Francois Poilpret
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package net.guts.gui.resource;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.Icon;
+
+/**
+ * TODO
+ *
+ * @author Jean-Francois Poilpret
+ */
+public interface ResourceConverter<T>
+{
+	public T convert(String value);
+}
+
+// All default Resource Converters are there:
+
+class StringConverter implements ResourceConverter<String>
+{
+	@Override public String convert(String value)
+	{
+		return value;
+	}
+}
+
+class IntConverter implements ResourceConverter<Integer>
+{
+	@Override public Integer convert(String value)
+	{
+		return null;
+	}
+}
+
+class ColorConverter implements ResourceConverter<Color>
+{
+	@Override public Color convert(String value)
+	{
+		//TODO
+		return null;
+	}
+}
+
+class FontConverter implements ResourceConverter<Font>
+{
+	@Override public Font convert(String value)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+class IconConverter implements ResourceConverter<Icon>
+{
+	@Override public Icon convert(String value)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+
---
+++ guts-gui/src/main/java/net/guts/gui/resource/ResourceModule.java
@@ -0,0 +1,49 @@
+//  Copyright 2009 Jean-Francois Poilpret
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package net.guts.gui.resource;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+
+/**
+ * TODO
+ *
+ * @author Jean-Francois Poilpret
+ */
+public final class ResourceModule extends AbstractModule
+{
+	/* (non-Javadoc)
+	 * @see com.google.inject.AbstractModule#configure()
+	 */
+	@Override protected void configure()
+	{
+		// Bind ResourceConverters for various default types: String, boolean, int, Color...
+		bindConverter(String.class, StringConverter.class);
+		bindConverter(Integer.class, IntConverter.class);
+		bindConverter(Color.class, ColorConverter.class);
+		bindConverter(Font.class, FontConverter.class);
+		
+		//TODO
+		// Add type listener for automatic injection of GUI
+	}
+	
+	private <T> void bindConverter(Class<T> type, Class<? extends ResourceConverter<T>> converter)
+	{
+		Resources.bindConverter(binder(), type).to(converter).asEagerSingleton();
+	}
+}
