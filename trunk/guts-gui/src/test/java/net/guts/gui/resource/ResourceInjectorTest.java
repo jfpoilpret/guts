5b2c7bf8e50d93b1ba86e1ddb6a7719cd01ed792
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2009-09-28 15:03:11 +0000

    Added more test cases for ResourceInjector.

--- trunk/guts-gui/src/test/java/net/guts/gui/resource/ResourceInjectorTest.java
+++ trunk/guts-gui/src/test/java/net/guts/gui/resource/ResourceInjectorTest.java
@@ -14,7 +14,11 @@
 
 package net.guts.gui.resource;
 
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.fest.assertions.Assertions;
 import org.testng.annotations.Test;
@@ -30,6 +34,83 @@ public class ResourceInjectorTest
 	
 	public void checkTextInjectionOneComponent()
 	{
+		JLabel label = createAndInjectLabel("test1-label");
+		// Check injection has worked
+		Assertions.assertThat(label.getText()).as("label.text").isEqualTo("LABEL-TEXT");
+		Assertions.assertThat(label.getToolTipText()).as("label.toolTipText").isEqualTo("LABEL-TOOLTIP");
+	}
+	
+	public void checkBooleanInjectionOneComponent()
+	{
+		JLabel label = createAndInjectLabel("test2-label");
+		// Check injection has worked
+		Assertions.assertThat(label.isEnabled()).as("label.enabled").isFalse();
+	}
+	
+	public void checkIntInjectionOneComponent()
+	{
+		JLabel label = createAndInjectLabel("test3-label");
+		// Check injection has worked
+		Assertions.assertThat(label.getIconTextGap()).as("label.iconTextGap").isEqualTo(2);
+	}
+	
+	public void checkColorInjectionOneComponent()
+	{
+		JLabel label = createAndInjectLabel("test4-label");
+		// Check injection has worked
+		Assertions.assertThat(label.getForeground()).as("label.foreground").isEqualTo(new Color(0x4080C0));
+	}
+	
+	public void checkFontInjectionOneComponent()
+	{
+		JLabel label = createAndInjectLabel("test5-label");
+		// Check injection has worked
+		Assertions.assertThat(label.getFont()).as("label.font").isEqualTo(Font.decode("Dialog-BOLD-14"));
+	}
+	
+//	label.setCursor(cursor);
+//	label.setIcon(icon);
+	//TODO test component injection of Icon
+	//TODO test component injection of Cursor
+	//TODO test mnemonics handling for text injection
+	//TODO test component injection of Alpha Color
+
+	public void checkInjectionHierarchy()
+	{
+		ResourceInjector injector = createInjector();
+		JPanel panel = new JPanel();
+		panel.setName(NAME + "-hierarchy");
+		JLabel label1 = createLabel("hierarchy-label1");
+		panel.add(label1);
+		JLabel label2 = createLabel("hierarchy-label2");
+		panel.add(label2);
+		JLabel label3 = createLabel("hierarchy-label3");
+		panel.add(label3);
+		injector.injectHierarchy(panel);
+		// Check injection has worked on every component in panel
+		Assertions.assertThat(panel.getForeground()).as("panel.foreground").isEqualTo(new Color(0x4080C0));
+		Assertions.assertThat(label1.getText()).as("label1.text").isEqualTo("LABEL1");
+		Assertions.assertThat(label2.getText()).as("label2.text").isEqualTo("LABEL2");
+		Assertions.assertThat(label3.getText()).as("label3.text").isEqualTo("LABEL3");
+	}
+	
+	static private JLabel createLabel(String name)
+	{
+		JLabel label = new JLabel();
+		label.setName(NAME + "-" + name);
+		return label;
+	}
+	
+	static private JLabel createAndInjectLabel(String name)
+	{
+		ResourceInjector resourceInjector = createInjector();
+		JLabel label = createLabel(name);
+		resourceInjector.injectComponent(label);
+		return label;
+	}
+	
+	static private ResourceInjector createInjector()
+	{
 		Injector injector = Guice.createInjector(new ResourceModule(), new AbstractModule()
 		{
 			@Override protected void configure()
@@ -38,26 +119,6 @@ public class ResourceInjectorTest
 			}
 		});
 		ResourceInjector resourceInjector = injector.getInstance(ResourceInjector.class);
-		JLabel label = new JLabel();
-		label.setName(NAME + "-test1-label");
-		resourceInjector.injectComponent(label);
-		// Check injection has worked
-//		label.setForeground(fg);
-//		label.setCursor(cursor);
-//		label.setDisplayedMnemonicIndex(index);
-//		label.setEnabled(true);
-//		label.setFont(font);
-//		label.setIcon(icon);
-		Assertions.assertThat(label.getText()).as("label.text").isEqualTo("LABEL-TEXT");
-		Assertions.assertThat(label.getToolTipText()).as("label.toolTipText").isEqualTo("LABEL-TOOLTIP");
+		return resourceInjector;
 	}
-	
-	//TODO test component injection of boolean
-	//TODO test component injection of int
-	//TODO test component injection of Color
-	//TODO test component injection of Font
-	//TODO test component injection of Icon
-	//TODO test component injection of Cursor
-	//TODO test mnemonics handling for text injection
-	//TODO test hierarchy injection (text only)
 }
