c3a5142eaf61515747ae545c8920b2ddde36dfbd
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2009-10-23 00:09:38 +0000

    Removed cglib dependency for resource injection, replaced with guts-common new property reflection support.

--- trunk/guts-gui/src/main/java/net/guts/gui/resource/ComponentInjector.java
+++ trunk/guts-gui/src/main/java/net/guts/gui/resource/ComponentInjector.java
@@ -16,13 +16,16 @@ package net.guts.gui.resource;
 
 import java.awt.Component;
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.sf.cglib.beans.BeanMap;
+import net.guts.common.bean.UntypedProperty;
+import net.guts.common.bean.UntypedPropertyFactory;
+import static net.guts.common.type.PrimitiveHelper.*;
+
+import com.google.inject.Inject;
+
 
 //TODO find a better name like SingleResourceInjector? InstanceResourcesInjector?...
 /**
@@ -44,37 +47,35 @@ class BeanPropertiesInjector implements ComponentInjector<Component>
 {
 	static final private Logger _logger = 
 		LoggerFactory.getLogger(BeanPropertiesInjector.class);
+
+	@Inject BeanPropertiesInjector(UntypedPropertyFactory properties)
+	{
+		_properties = properties;
+	}
 	
 	@Override public void inject(Component component, ResourceMap resources)
 	{
 		String prefix = component.getName();
-		Class<?> componentType = component.getClass();
+		Class<? extends Component> componentType = component.getClass();
 		if (prefix == null)
 		{
 			String msg = String.format("Component has no name: %d", component);
 			_logger.info(msg);
 			return;
 		}
-		// Get the right BeanMap for that component
-		BeanMap bean = _beans.get(componentType);
-		if (bean == null)
-		{
-			bean = BeanMap.create(component);
-			bean.setBean(null);
-			_beans.put(componentType, bean);
-		}
 		// For each injectable resource
 		for (ResourceMap.Key key: resources.keys(prefix))
 		{
-			// Check that this property exists
-			Class<?> type = bean.getPropertyType(key.key());
-			if (type == null)
+			// Use FEST-Reflect & Check that this property exists
+			UntypedProperty property = _properties.property(key.key(), componentType);
+			if (property == null || !property.isWritable())
 			{
-				String msg = String.format("Unknow property `%s` of type `%s`", 
-					key, componentType);
+				String msg = String.format("Unknown property `%s` of type `%s`", 
+					key.key(), componentType);
 				_logger.info(msg);
 				continue;
 			}
+			Class<?> type = property.type();
 			// Get the value in the correct type
 			Object value = resources.getValue(key, type);
 			// Special handling for mnemonics
@@ -83,29 +84,43 @@ class BeanPropertiesInjector implements ComponentInjector<Component>
 				MnemonicInfo info = MnemonicInfo.extract((String) value);
 				if (info != null)
 				{
-					//FIXME what if component doesn't have mnemonic or displayedMnemonicIndex property?
 					// Set the property with the resource value
-					bean.put(component, "text", info.getText());
-					bean.put(component, "displayedMnemonic", info.getMnemonic());
-					bean.put(component, "displayedMnemonicIndex", info.getMnemonicIndex());
+					setProperty(component, "text", info.getText());
+					setProperty(component, "displayedMnemonic", info.getMnemonic());
+					setProperty(component, "displayedMnemonicIndex", info.getMnemonicIndex());
 				}
 				else
 				{
 					// Set the property with the resource value
-					bean.put(component, "text", value);
-					bean.put(component, "displayedMnemonic", KeyEvent.VK_UNDEFINED);
-					bean.put(component, "displayedMnemonicIndex", -1);
+					setProperty(component, "text", (String) value);
+					setProperty(component, "displayedMnemonic", KeyEvent.VK_UNDEFINED);
+					setProperty(component, "displayedMnemonicIndex", -1);
 				}
 			}
 			else
 			{
 				// Set the property with the resource value
-				bean.put(component, key.key(), value);
+				property.set(component, value);
 			}
 		}
 	}
-
-	//FIXME probably not a terrific idea:
-	// - requires cglib in classpath (in addition to "guice cglib")
-	final private Map<Class<?>, BeanMap> _beans = new HashMap<Class<?>, BeanMap>();
+	
+	private void setProperty(Component component, String name, Object value)
+	{
+		Class<?> type = component.getClass();
+		UntypedProperty property = _properties.property(name, type);
+		if (property == null || !property.isWritable())
+		{
+			//TODO log something
+			return;
+		}
+		if (value != null && !toWrapper(property.type()).isAssignableFrom(value.getClass()))
+		{
+			//TODO log something
+			return;
+		}
+		property.set(component, value);
+	}
+	
+	final private UntypedPropertyFactory _properties;
 }
