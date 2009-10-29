b775e34718ff943a3054ed7cc97ce35d6fa69666
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2009-10-29 16:00:37 +0000

    Removed TypeHelper after refactoring its methods into guts-common TypeHelper.

--- guts-gui/src/main/java/net/guts/gui/resource/ResourceInjectorImpl.java
+++ guts-gui/src/main/java/net/guts/gui/resource/ResourceInjectorImpl.java
@@ -21,6 +21,8 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.guts.common.type.TypeHelper;
+
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
