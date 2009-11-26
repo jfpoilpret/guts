300c0f155fef82309f4fedf584559f3458d31942
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2009-11-26 13:48:31 +0000

    Added javadoc for new ResourcePreprocessor feature.

--- trunk/guts-gui/src/main/java/net/guts/gui/resource/ResourcePreprocessor.java
+++ trunk/guts-gui/src/main/java/net/guts/gui/resource/ResourcePreprocessor.java
@@ -16,8 +16,46 @@ package net.guts.gui.resource;
 
 import com.google.inject.ImplementedBy;
 
+/**
+ * When bound to Guts-GUI (with {@link Resources#bindResourcePreprocessor}), an 
+ * implementation of this interface will be called on any property value, before it
+ * is converted through {@link ResourceConverter}.
+ * <p/>
+ * This allows performing any necessary processing to the "raw" string value of
+ * resource properties.
+ * <p/>
+ * Default Guts-GUI implementation of {@code ResourcePreprocessor} performs symbol
+ * substitution, i.e. any occurrence of {@code $&#123;name&#125;} is replaced by the 
+ * value of the property named {@code name} if it exists, else the occurrence 
+ * remains intact.
+ * <p/>
+ * This enables you to define commonly used values in one bundle and use them in
+ * another bundle, as in the following example:
+ * <pre>
+ * # in root bundle
+ * application-version = 1.0
+ * ...
+ * # in another bundle
+ * about-dialog-version-label.text=${application-version}
+ * </pre>
+ * <p/>
+ * You can override default behavior by providing your own implementation and 
+ * binding it to Guts-GUI by using {@link Resources#bindResourcePreprocessor}.
+ * 
+ * @author Jean-Francois Poilpret
+ */
 @ImplementedBy(SymbolSubstitutionResourcePreprocessor.class)
 public interface ResourcePreprocessor
 {
+	/**
+	 * Convert the raw string value {@code source} of a resource bundle property
+	 * into a processed string value, ready to be converted and injected into a 
+	 * component.
+	 * 
+	 * @param map the {@code ResourceMap} from which {@code source} is issued; you
+	 * can use it to lookup other properties.
+	 * @param source the raw value of a resource property
+	 * @return the processed value for {@code source}
+	 */
 	public String convert(ResourceMap map, String source);
 }
