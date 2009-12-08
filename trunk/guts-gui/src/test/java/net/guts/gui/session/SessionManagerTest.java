b7a921e9b326c2c5d8d71fdf171b1299e2badf00
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2009-12-08 15:08:47 +0000

    Added new TC for session management.

--- trunk/guts-gui/src/test/java/net/guts/gui/session/SessionManagerTest.java
+++ trunk/guts-gui/src/test/java/net/guts/gui/session/SessionManagerTest.java
@@ -17,6 +17,11 @@ package net.guts.gui.session;
 import java.awt.Window;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
 
 import org.fest.assertions.Assertions;
 import org.testng.annotations.Test;
@@ -42,6 +47,101 @@ public class SessionManagerTest
 		Assertions.assertThat(window.getY()).as("window.y").isEqualTo(10);
 		Assertions.assertThat(window.getWidth()).as("window.width").isEqualTo(400);
 		Assertions.assertThat(window.getHeight()).as("window.height").isEqualTo(300);
+	}
+	
+	public void checkWindowStateNotRestoredIfNotSavedBefore()
+	{
+		SessionManager manager = manager();
+		Window window = window("checkWindowStateNotRestoredIfNotSavedBefore");
+		window.setBounds(20, 10, 400, 300);
+		manager.restore(window);
+		
+		Assertions.assertThat(window.getX()).as("window.x").isEqualTo(20);
+		Assertions.assertThat(window.getY()).as("window.y").isEqualTo(10);
+		Assertions.assertThat(window.getWidth()).as("window.width").isEqualTo(400);
+		Assertions.assertThat(window.getHeight()).as("window.height").isEqualTo(300);
+	}
+	
+	public void checkSplitPane()
+	{
+		SessionManager manager = manager();
+		Window window = window("checkSplitPane");
+		JSplitPane split = initSplitPaneWindow(window);
+		
+		int divider = window.getWidth() / 4;
+		split.setDividerLocation(divider);
+		manager.save(window);
+
+		window = window("checkSplitPane");
+		split = initSplitPaneWindow(window);
+		manager.restore(window);
+
+		Assertions.assertThat(split.getDividerLocation()).as("split.divider").isEqualTo(divider);
+	}
+	
+	private JSplitPane initSplitPaneWindow(Window window)
+	{
+		JSplitPane split = new JSplitPane();
+		split.setLeftComponent(new JLabel("LEFT"));
+		split.setRightComponent(new JLabel("RIGHT"));
+		window.add(split);
+		window.pack();
+		return split;
+	}
+	
+	public void checkTable()
+	{
+		SessionManager manager = manager();
+		Window window = window("checkTable");
+		JTable table = initTableWindow(window);
+
+		TableColumn column = table.getColumnModel().getColumn(0);
+		table.removeColumn(column);
+		int width = table.getColumnModel().getColumn(0).getWidth();
+		manager.save(window);
+
+		window = window("checkTable");
+		table = initTableWindow(window);
+		manager.restore(window);
+
+		Assertions.assertThat(table.getColumnModel().getColumn(0).getWidth()).as("table.column[0].width").isEqualTo(width);
+	}
+	
+	private JTable initTableWindow(Window window)
+	{
+		Object[] columns = {"First column", "C2"};
+		Object[][] rows = {{23, 32}, {14, 41}, {67, 76}};
+		JTable table = new JTable(rows, columns);
+		window.add(table);
+		window.pack();
+		return table;
+	}
+
+	public void checkTabbedPane()
+	{
+		SessionManager manager = manager();
+		Window window = window("checkTabbedPane");
+		JTabbedPane tabs = initTabWindow(window);
+
+		Assertions.assertThat(tabs.getSelectedIndex()).as("tabs.selected").isEqualTo(0);
+		tabs.setSelectedIndex(1);
+		manager.save(window);
+		
+		window = window("checkTabbedPane");
+		tabs = initTabWindow(window);
+		manager.restore(window);
+		
+		Assertions.assertThat(tabs.getSelectedIndex()).as("tabs.selected").isEqualTo(1);
+	}
+
+	private JTabbedPane initTabWindow(Window window)
+	{
+		JTabbedPane tabs = new JTabbedPane();
+		tabs.addTab("First Tab", new JLabel("ONE"));
+		tabs.addTab("Tab 2", new JLabel("TWO"));
+		window.add(tabs);
+		window.pack();
+		return tabs;
 	}
 	
 	private SessionManager manager()
