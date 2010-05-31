4c5580be99af871b238ea7f1e5dfb4a793df18b0
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2010-05-31 14:35:12 +0000

    Fixed problem with JApplet bounds restoration (in AppletViewer).

--- trunk/guts-gui/src/main/java/net/guts/gui/session/SessionState.java
+++ trunk/guts-gui/src/main/java/net/guts/gui/session/SessionState.java
@@ -155,7 +155,9 @@ abstract class ScreenEstateState<T extends Component> implements SessionState<T>
 		Rectangle bounds = (_estate != null ? _estate.bounds() : null);
 		if (bounds != null)
 		{
-			component.setBounds(bounds);
+			// Don't use setBounds() because it doesn't seem to work with JApplet!
+			component.setLocation(bounds.getLocation());
+			component.setSize(bounds.getSize());
 		}
 	}
 	
