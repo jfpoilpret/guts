b83858a0edf339ddee9a07b71475595772583e17
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2010-06-29 16:07:51 +0000

    First tests of new guts-flexdock module within AddressBook sample app. Will need further improvements.

---
+++ trunk/guts-gui-example/src/main/java/net/guts/gui/examples/addressbook/docking/AddressBookDockLifecycleStarter.java
@@ -0,0 +1,67 @@
+//  Copyright 2009 Jean-Francois Poilpret
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package net.guts.gui.examples.addressbook.docking;
+
+import javax.swing.JFrame;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.guts.gui.application.docking.DockingLifecycle;
+
+import net.guts.gui.examples.addressbook.AddressBookMenuBar;
+import net.guts.gui.exception.HandlesException;
+import net.guts.gui.message.MessageFactory;
+
+import com.google.inject.Inject;
+
+public class AddressBookDockLifecycleStarter extends DockingLifecycle
+{
+	static private final Logger _logger = 
+		LoggerFactory.getLogger(AddressBookDockLifecycleStarter.class);
+	
+	@Inject
+	public AddressBookDockLifecycleStarter(
+		AddressBookMenuBar menuBar,
+		MessageFactory messageFactory)
+	{
+		_menuBar = menuBar;
+		_messageFactory = messageFactory;
+	}
+	
+	/* (non-Javadoc)
+	 * @see net.guts.gui.application.docking.DockingLifecycle#initMainFrame(javax.swing.JFrame)
+	 */
+	@Override protected void initMainFrame(JFrame frame)
+	{
+		frame.setJMenuBar(_menuBar);
+	}
+	
+	// CSOFF: GenericIllegalRegexp
+	// Handle exceptions on the EDT
+	@HandlesException
+	public boolean handle(Throwable e)
+	{
+		// Log the exception
+		_logger.info("Exception has occurred!", e);
+		// Show Message to the end user
+		_messageFactory.showMessage("unexpected-error", e, e.getMessage());
+		return true;
+	}
+	// CSON: GenericIllegalRegexp
+
+	final private AddressBookMenuBar _menuBar;
+	final private MessageFactory _messageFactory;
+}
