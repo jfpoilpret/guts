7621341c521de8df648e886a9debcb9c52929271
trumpetinc <trumpetinc@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2010-08-18 02:48:37 +0000

    Exploring different dialog strategies

---
+++ trunk/guts-gui/src/main/java/net/guts/gui/dialogalt/ViewCloser.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Aug 17, 2010
+ * (c) 2010 Trumpet, Inc.
+ *
+ */
+package net.guts.gui.dialogalt;
+
+/**
+ * @author kevin
+ */
+public interface ViewCloser {
+    /**
+     * 
+     * @return true if the view closed, false otherwise
+     */
+    public boolean doClose();
+}
