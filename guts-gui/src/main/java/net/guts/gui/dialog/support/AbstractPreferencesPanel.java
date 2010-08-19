ea3bbcd45292b550b4a425e98de77010b4d8a172
trumpetinc <trumpetinc@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2010-08-19 14:07:25 +0000

    Simplify code a bit, add delegation of revert actions

--- guts-gui/src/main/java/net/guts/gui/dialog/support/AbstractPreferencesPanel.java
+++ guts-gui/src/main/java/net/guts/gui/dialog/support/AbstractPreferencesPanel.java
@@ -8,7 +8,9 @@ package net.guts.gui.dialog.support;
 import java.awt.BorderLayout;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.Action;
 import javax.swing.DefaultListModel;
@@ -37,6 +39,7 @@ public class AbstractPreferencesPanel extends AbstractMultiPanel {
     private final JPanel viewContainer = new JPanel(new BorderLayout());
     private final JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
     private final DefaultListModel listModel = new DefaultListModel();
+    private final List<ViewRegistration> registrations = new ArrayList<ViewRegistration>();
     private final JList optionsList = new JList(listModel);
     
     public AbstractPreferencesPanel() {
@@ -49,50 +52,82 @@ public class AbstractPreferencesPanel extends AbstractMultiPanel {
         
     }
 
-//    protected void setupActions(List<GutsAction> actions) {
-//        actions.add(getAcceptAction());
-//        actions.add(getCancelAction());
-//    }
-    
+    /**
+     * Registers a preferences component to be displayed in the preferences list.  The name of the component
+     * will be used as the list display (probably want to change this?? To what??).
+     * @param component the preferences component to register
+     * @param applyAction the action that will be called when the OK button is clicked (the OK button enablement state is bound to the enablement state of all registered apply actions)
+     * @param revertAction the action that will be called when the Cancel button is clicked (the Cancel button is always enabled as it can be used to close the dialog)
+     */
     protected void register(JComponent component, Action applyAction, Action revertAction){
         ViewRegistration registration = new ViewRegistration(component.getName(), component, applyAction, revertAction, new ActionBinder(applyAction)); 
+        registrations.add(registration);
         listModel.addElement(registration);
     }
 
+    /**
+     * @see net.guts.gui.dialog.support.AbstractPanel#getAcceptAction()
+     */
     protected GutsAction getAcceptAction() {
         return accept;
     }
     
     private final GutsAction accept = new GutsAction("ok"){
         protected void perform() {
-            
-            for (Action action : new MyIterable<ViewRegistration, Action>(listModel, ViewRegistration.applyActionFunction)) {
-                if (action != null){
-                    if (!action.isEnabled())
-                        throw new IllegalStateException("Action " + action + " is not enabled");
+            for (ViewRegistration registration : registrations) {
+                Action action = registration.applyAction;
+                if (action != null && action.isEnabled()){
                     action.actionPerformed(this.event());
                 }
-                    
             }
-            
         }  
     };
     
-    private void syncEnabled(){
-        accept.action().setEnabled(allApplyActionsAreEnabled());
+    protected GutsAction getCancelAction() {
+        return revert;
     }
     
-    private boolean allApplyActionsAreEnabled(){
-        for (Action action : new MyIterable<ViewRegistration, Action>(listModel, ViewRegistration.applyActionFunction)) {
-            if (action != null && !action.isEnabled()){
-                return false;
+    private final GutsAction revert = new GutsAction("cancel"){
+        protected void perform() {
+            for (ViewRegistration registration : registrations) {
+                Action action = registration.revertAction;
+                if (action != null && action.isEnabled()){
+                    action.actionPerformed(this.event());
+                }
+            }
+            
+            getParentDialog().close(true);
+        }  
+    };
+   
+    
+    /**
+     * Synchronizes global action enablement with the enablement state of actions from registered preferences panels
+     * If one or more registered apply actions are enabled, then the global apply action is enabled
+     */
+    private void syncEnabled(){
+        accept.action().setEnabled(atLeastOneApplyActionsAreEnabled());
+    }
+    
+    private boolean atLeastOneApplyActionsAreEnabled(){
+        for (ViewRegistration registration : registrations) {
+            Action action = registration.applyAction;
+            if (action != null && action.isEnabled()){
+                return true;
             }
         }
-        return true;
+        return false;
     }
-    
+
+    /**
+     * @see net.guts.gui.dialog.support.AbstractMultiPanel#getSubComponents()
+     */
     final protected Iterable<JComponent> getSubComponents() {
-        return new MyIterable<ViewRegistration, JComponent>(listModel, ViewRegistration.componentFunction);
+        List<JComponent> components = new ArrayList<JComponent>(registrations.size());
+        for (ViewRegistration registration : registrations) {
+            components.add(registration.view);
+        }
+        return components;
     }
     
     private void bindListSelectionToViewContainer(){
@@ -113,6 +148,10 @@ public class AbstractPreferencesPanel extends AbstractMultiPanel {
         
     }
     
+    /**
+     * Allows top-level action enablement to be adjusted when the actions of registered preferences panels
+     * are enabled/disabled.
+     */
     private class ActionBinder implements PropertyChangeListener{
         private final Action source;
         public ActionBinder(Action source){
@@ -140,46 +179,17 @@ public class AbstractPreferencesPanel extends AbstractMultiPanel {
         }
     }
     
-    private static class MyIterable<E, V> implements Iterable<V>, Iterator<V>{
 
-        private final ListModel model;
-        private final Function<E, V> function;
-        
-        private int index = 0;
-        
-        public MyIterable(ListModel model, Function<E, V> function) {
-            this.model = model;
-            this.function = function;
-        }
-        
-        public Iterator<V> iterator() {
-            return this;
-        }
 
-        public boolean hasNext() {
-            return index < model.getSize();
-        }
-
-        public V next() {
-            return function.evaluate(((E)model.getElementAt(index++)));
-        }
-
-        public void remove() {
-            throw new UnsupportedOperationException("remove() not supported");
-        }
-        
-    }
-    
-    private static interface Function<A, B>{
-        public B evaluate(A in);
-    }
-    
+    /**
+     * Represents a registered preferences view
+     */
     private static class ViewRegistration{
         public final String name;
         public final JComponent view;
-        private final Action applyAction;
-        private final Action revertAction;
-        private final ActionBinder actionBinder;
+        public final Action applyAction;
+        public final Action revertAction;
+        public final ActionBinder actionBinder;
         
         public ViewRegistration(String name, JComponent view, Action applyAction, Action revertAction, ActionBinder actionBinder){
             this.name = name;
@@ -192,30 +202,6 @@ public class AbstractPreferencesPanel extends AbstractMultiPanel {
         public String toString() {
             return name;
         }
-        
-        public static Function<ViewRegistration, JComponent> componentFunction = new Function<ViewRegistration, JComponent>(){
-            public JComponent evaluate(ViewRegistration reg) {
-                return reg.view;
-            }
-        };
-        
-        public static Function<ViewRegistration, Action> applyActionFunction = new Function<ViewRegistration, Action>(){
-            public Action evaluate(ViewRegistration reg) {
-                return reg.applyAction;
-            }
-        };
-
-        public static Function<ViewRegistration, Action> revertActionFunction = new Function<ViewRegistration, Action>(){
-            public Action evaluate(ViewRegistration reg) {
-                return reg.applyAction;
-            }
-        };
-
-        public static Function<ViewRegistration, ActionBinder> actionBinderFunction = new Function<ViewRegistration, ActionBinder>(){
-            public ActionBinder evaluate(ViewRegistration reg) {
-                return reg.actionBinder;
-            }
-        };
 
     }
 }
