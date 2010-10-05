06e0ad6cdc423b94345d9578bfe3934db2bfde2b
Andrei.Pozolotin <Andrei.Pozolotin@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2010-10-05 22:14:03 +0000

    resolve: http://kenai.com/jira/browse/GUTS-44

--- trunk/guts-base/guts-gui/src/main/java/net/guts/gui/session/XStreamSerializationManager.java
+++ trunk/guts-base/guts-gui/src/main/java/net/guts/gui/session/XStreamSerializationManager.java
@@ -14,28 +14,39 @@
 
 package net.guts.gui.session;
 
+import java.nio.charset.Charset;
+
 import com.google.inject.Singleton;
 import com.thoughtworks.xstream.XStream;
 
 @Singleton
-class XStreamSerializationManager implements SerializationManager
-{
-	@Override public byte[] serialize(Object object)
-	{
+class XStreamSerializationManager implements SerializationManager {
+
+	/*
+	 * http://kenai.com/jira/browse/GUTS-44
+	 * 
+	 * UTF-8 is always present:
+	 * 
+	 * http://download.oracle.com/javase/6/docs/api/java/nio/charset/Charset.html
+	 */
+	static private final Charset UTF_8 = Charset.forName("UTF-8");
+
+	@Override
+	public byte[] serialize(Object object) {
 		// Serialize content into XML
 		String xml = _xstream.toXML(object);
-		return xml.getBytes();
+		return xml.getBytes(UTF_8);
 	}
-	
-	@Override public void deserialize(byte[] content, Object object)
-	{
-		if (content != null)
-		{
-			String xml = new String(content);
+
+	@Override
+	public void deserialize(byte[] content, Object object) {
+		if (content != null) {
+			String xml = new String(content, UTF_8);
 			_xstream.fromXML(xml, object);
 		}
-		
+
 	}
-	
+
 	final private XStream _xstream = new XStream();
+
 }
