411c3205edd7d36ee0e4c5b51e70e36e280a2565
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2011-02-14 20:34:24 +0000

    Refactored MapConverter into from sandbox into guts-gui resources package.

--- trunk/guts-base/guts-gui/src/main/java/net/guts/gui/resource/ResourceConverter.java
+++ trunk/guts-base/guts-gui/src/main/java/net/guts/gui/resource/ResourceConverter.java
@@ -24,6 +24,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -348,4 +350,32 @@ class ListConverter<T> extends AbstractCompoundResourceConverter<List<T>>
 
 	final private TypeLiteral<T> _type;
 	final private String _delimiters;
+}
+
+class MapConverter<K, V> extends AbstractCompoundResourceConverter<Map<K, V>>
+{
+	MapConverter(TypeLiteral<K> keyType, TypeLiteral<V> valueType)
+	{
+		_keyType = keyType;
+		_valueType = valueType;
+	}
+	
+	@Override public Map<K, V> convert(ResourceEntry entry)
+	{
+		Map<K, V> map = new HashMap<K, V>();
+		ResourceConverter<K> keyConverter = converter(_keyType);
+		ResourceConverter<V> valueConverter = converter(_valueType);
+		Matcher matcher = _regexp.matcher(entry.value());
+		while (matcher.find())
+		{
+			K key = keyConverter.convert(entry.derive(matcher.group(1)));
+			V value = valueConverter.convert(entry.derive(matcher.group(2)));
+			map.put(key, value);
+		}
+		return map;
+	}	
+
+	final private Pattern _regexp = Pattern.compile("\\[([^:]+):([^\\]]*)\\] *");
+	final private TypeLiteral<K> _keyType;
+	final private TypeLiteral<V> _valueType;
 }
