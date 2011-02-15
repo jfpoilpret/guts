08e144f8f5d420a5fa899c36ba71f82303148b1a
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2011-02-15 21:14:28 +0000

    Added some javadoc, in preparation of  promotion to guts-gui

--- guts-sandbox/guts-dialog-template/src/main/java/net/guts/gui/dialog2/DialogFactory.java
+++ guts-sandbox/guts-dialog-template/src/main/java/net/guts/gui/dialog2/DialogFactory.java
@@ -21,10 +21,50 @@ import net.guts.gui.window.RootPaneConfig;
 
 import com.google.inject.ImplementedBy;
 
+/**
+ * Interface defining the factory to create and show modal dialogs in the
+ * application.
+ * <p/>
+ * This service is directly injectable through Guice facilities.
+ * 
+ * @author Jean-Francois Poilpret
+ */
 @ImplementedBy(DialogFactoryImpl.class)
 public interface DialogFactory
 {
+	/**
+	 * Shows a modal dialog.
+	 * <p/>
+	 * The passed {@code view} must have a unique name (set by 
+	 * {@link javax.swing.JComponent#setName(String)}.
+	 * <p/>
+	 * The passed {@code config} must be obtained by 
+	 * {@link net.guts.gui.window.JDialogConfig} and can possibly be 
+	 * {@link net.guts.gui.window.JDialogConfig#merge merged} with other configuration
+	 * parameters provided by more specialized configuration builders, such as:
+	 * <ul>
+	 * <li>{@link net.guts.gui.dialog2.template.okcancel.OkCancel}</li>
+	 * <li>{@link net.guts.gui.dialog2.template.wizard.Wizard}</li>
+	 * </ul>
+	 * This additional configuration sets up decoration of {@code view} before adding
+	 * it to a new {@link JDialog}.
+	 * 
+	 * @param view panel to be contained in the shown dialog
+	 * @param config the configuration for displaying {@code view} in a new {@link JDialog};
+	 * this must be created with {@link net.guts.gui.window.JDialogConfig} builder class.
+	 */
 	public void showDialog(JComponent view, RootPaneConfig<JDialog> config);
 
+	/**
+	 * Shows a modal dialog. The contained panel is directly instantiated by the
+	 * method through Guice (automatically injected), based on the passed Class.
+	 * <p/>
+	 * The rules and comments for instances of {@code clazz} are the same as for
+	 * {@link #showDialog(JComponent, RootPaneConfig)}.
+	 * 
+	 * @param view class of the panel to be contained in the shown dialog
+	 * @param config the configuration for displaying {@code view} in a new {@link JDialog};
+	 * this must be created with {@link net.guts.gui.window.JDialogConfig} builder class.
+	 */
 	public void showDialog(Class<? extends JComponent> view, RootPaneConfig<JDialog> config);
 }
