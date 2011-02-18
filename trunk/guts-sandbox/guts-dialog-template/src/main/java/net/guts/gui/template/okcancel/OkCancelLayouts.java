76e7d56b07aaf066a2d310802d65b362de0cb111
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2011-02-18 19:14:15 +0000

    Added some javadoc, in preparation of  promotion to guts-gui

--- trunk/guts-sandbox/guts-dialog-template/src/main/java/net/guts/gui/template/okcancel/OkCancelLayouts.java
+++ trunk/guts-sandbox/guts-dialog-template/src/main/java/net/guts/gui/template/okcancel/OkCancelLayouts.java
@@ -21,12 +21,40 @@ import com.google.inject.TypeLiteral;
 import com.google.inject.binder.LinkedBindingBuilder;
 import com.google.inject.multibindings.MapBinder;
 
+/**
+ * Utility class to define, from within a Guice {@link com.google.inject.Module},
+ * bindings for {@link OkCancelLayoutAdder}s.
+ * <p/>
+ * {@link OkCancelLayoutAdder}s allow {@link OkCancel} template to "optimize"
+ * the addition of buttons to a view, based on the {@link LayoutManager} that
+ * handles this view.
+ * 
+ * @author jfpoilpret
+ **/
 public final class OkCancelLayouts
 {
 	private OkCancelLayouts()
 	{
 	}
 	
+	/**
+	 * Initializes a binding between a given {@link LayoutManager} subtype
+	 * and a matching {@link OkCancelLayoutAdder}.
+	 * <p/>
+	 * This is based on usual Guice EDSL for bindings:
+	 * <pre>
+	 * OkCancelLayouts.bindLayout(binder(), MigLayout.class)
+	 *     .to(OkCancelMigLayoutAdder.class);
+	 * </pre>
+	 * <p/>
+	 * This must be called from {@link com.google.inject.Module#configure(Binder)}.
+	 * 
+	 * @param binder the Guice binder passed to 
+	 * {@link com.google.inject.Module#configure(Binder)}
+	 * @param layout {@link LayoutManager} type to bind to a {@link OkCancelLayoutAdder}
+	 * @return a {@link com.google.inject.binder.LinkedBindingBuilder} to bind 
+	 * {@code layout} to an {@link OkCancelLayoutAdder}
+	 */
 	static public LinkedBindingBuilder<OkCancelLayoutAdder> bindLayout(
 		Binder binder, Class<? extends LayoutManager> layout)
 	{
