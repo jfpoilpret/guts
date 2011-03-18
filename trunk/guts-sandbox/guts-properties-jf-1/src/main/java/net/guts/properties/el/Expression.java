a785c9ad9a71796d35022c30f080c0656fae77dc
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2011-03-18 18:48:35 +0000

    Removed useless API

--- trunk/guts-sandbox/guts-properties-jf-1/src/main/java/net/guts/properties/el/Expression.java
+++
@@ -1,7 +0,0 @@
-package net.guts.properties.el;
-
-public interface Expression<T>
-{
-	public T evaluate();
-	public Expression<T> set(String name, Object value);
-}
--- trunk/guts-sandbox/guts-properties-jf-1/src/main/java/net/guts/properties/el/ExpressionSource.java
+++
@@ -1,6 +0,0 @@
-package net.guts.properties.el;
-
-public interface ExpressionSource<T>
-{
-	public T evaluate(ExpressionContext context);
-}
