d99ccd32512b16be921136091af04a4790b36ee2
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2011-03-18 18:46:22 +0000

    Made last Bindings utility method type-safe.

---
+++ trunk/guts-sandbox/guts-properties-jf-1/src/main/java/net/guts/binding/GutsBindings.java
@@ -0,0 +1,25 @@
+package net.guts.binding;
+
+import javax.swing.JComponent;
+
+import net.guts.properties.Bean;
+
+import com.jgoodies.binding.adapter.Bindings;
+import com.jgoodies.binding.value.ValueModel;
+
+public class GutsBindings extends Bindings
+{
+	// Prevent direct instantiation
+	protected GutsBindings()
+	{
+	}
+	
+	//TODO:	usage: bind(field, Models.of(JTextField.class).getText(), textValueModel);
+	@SuppressWarnings("unchecked") 
+	public static <C extends JComponent, T> void bind(
+		C component, T getter, ValueModel<T> valueModel)
+	{
+		Bean<C> helper = Bean.create((Class<C>) component.getClass());
+		bind(component, helper.property(getter).name(), valueModel);
+	}
+}
