8c7e07d0799a40ce69975d8da437355b2e5a204c
jfpoilpret <jfpoilpret@ed87101c-5a2a-6b6b-bb8b-c470b4fc8f04>
2011-06-13 18:11:55 +0000

    Improved validation approach to make it generic and better reusable.

---
+++ trunk/guts-sandbox/guts-gui-mvpm-demo/src/main/java/net/guts/gui/validation/WpWindowValidation.java
@@ -0,0 +1,46 @@
+//  Copyright 2009 Jean-Francois Poilpret
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package net.guts.gui.validation;
+
+import java.awt.Window;
+
+import javax.swing.JComponent;
+import javax.swing.RootPaneContainer;
+
+import net.guts.gui.window.AbstractWindowProcessor;
+import net.guts.gui.window.RootPaneConfig;
+
+import com.jgoodies.validation.ValidationResultModel;
+
+class WpWindowValidation<V extends RootPaneContainer> 
+extends AbstractWindowProcessor<Window, V>
+{
+	WpWindowValidation()
+	{
+		super(Window.class);
+	}
+
+	@Override protected void processRoot(Window root, RootPaneConfig<V> config)
+	{
+		ValidationResultModel validation = config.get(ValidationResultModel.class);
+		if (validation != null)
+		{
+			RootPaneContainer container = (RootPaneContainer) root;
+			JComponent view = (JComponent) container.getContentPane();
+			JComponent feedback = new IconFeedbackPanel(validation, view);
+			container.setContentPane(feedback);
+		}
+	}
+}
