85ae5d2d704cbe2cc3423dbe15cc797b6342c7d2
Jean-François Poilpret <jfpoilpret@users.noreply.github.com>
2011-06-16 21:24:36 +0000

    Now ValidationModule checks that JGoodies Validation library is present in classpath.

--- guts-sandbox/guts-gui-mvpm-demo/src/main/java/net/guts/gui/validation/ValidationModule.java
+++ guts-sandbox/guts-gui-mvpm-demo/src/main/java/net/guts/gui/validation/ValidationModule.java
@@ -14,23 +14,62 @@
 
 package net.guts.gui.validation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.guts.gui.window.WindowProcessor;
 import net.guts.gui.window.Windows;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.assistedinject.FactoryProvider;
 
+/**
+ * Guice {@link com.google.inject.Module} for Guts-GUI Validation support, based
+ * on JGoodies Validation library.
+ * This module must be added to the list of modules passed to 
+ * {@link com.google.inject.Guice#createInjector}:
+ * <pre>
+ * Injector injector = Guice.createInjector(new ValidationModule(), ...);
+ * </pre>
+ * <p/>
+ * TODO
+ * 
+ * @author Jean-Francois Poilpret
+ */
 public final class ValidationModule extends AbstractModule
 {
+	static final private Logger _logger = LoggerFactory.getLogger(ValidationModule.class);
+	
 	@Override protected void configure()
 	{
-		bind(ValidationMessageFactory.class).toProvider(FactoryProvider.newFactory(
-			ValidationMessageFactory.class, ResourceValidationMessage.class));
-		requestStaticInjection(ValidationHelper.class);
-		Windows.bindWindowProcessor(binder(), WindowProcessor.SESSION_STORAGE + 1000)
-			.to(WpWindowValidation.class);
+		if (isJGoodiesValidationInClasspath())
+		{
+			bind(ValidationMessageFactory.class).toProvider(FactoryProvider.newFactory(
+				ValidationMessageFactory.class, ResourceValidationMessage.class));
+			requestStaticInjection(ValidationHelper.class);
+			Windows.bindWindowProcessor(binder(), WindowProcessor.SESSION_STORAGE + 1000)
+				.to(WpWindowValidation.class);
+		}
+		else
+		{
+			_logger.warn(
+				"JGoodies Validation library not in classpath, ValidationModule not used.");
+		}
 	}
 	
+	static boolean isJGoodiesValidationInClasspath()
+	{
+		try
+		{
+			Class.forName("com.jgoodies.validation.ValidationResult");
+			return true;
+		}
+		catch (ClassNotFoundException e)
+		{
+			return false;
+		}
+	}
+
 	@Override public boolean equals(Object other)
 	{
 		return other instanceof ValidationModule;
